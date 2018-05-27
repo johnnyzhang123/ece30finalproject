79bf87e3e1fd4789ca7735010b87fad1378601f7
krisstofu <32915455+krisstofu@users.noreply.github.com>
2018-05-27 16:29:46 -0700

    Update mips_template.s

--- mips_template.s
+++ mips_template.s
@@ -58,19 +58,20 @@ swap:
 	# Swap the elements at the given indices in the list
 
 	### INSERT YOUR CODE HERE
-	sll $t1,$a1,2  #multiply a1 by 4
-	sll $t2, $a2,2 #multiply a2 by 4
-	add $t1 $t1,$a0 #access the value that is stored in a1
-	add $t2, $t2,$a0# access value in a2
-	lw $t3,0($t1) #store value in t1 to t3
-	lw $t4,0($t2)# store value in t2 to t4
-	sw $t4,0($t1)#store value in t4 to t1
-	sw $t3,0($t2)#store value in t3 to t2
+	sll $s1, $a1, 2  	#multiply index 1 by 4
+	sll $s2, $a2, 2 	#multiply index 2 by 4
+	add $s1, $s1, $a0 	#access the value that is stored in a1
+	add $s2, $s2, $a0	#access value in a2
+	lw $t3, 0($s1) 		#store value in t1 to t3
+	lw $t4, 0($s2)		#store value in t2 to t4
+	sw $t4, 0($s1)		#store value in t4 to t1
+	sw $t3, 0($s2)		#store value in t3 to t2
 	# return to caller
 	jr $ra
 	##############################
 	#SWAP TEST SUCCESSFUL
 	#########################
+	
 ########################
 #   medianOfThree      #
 ########################
@@ -83,67 +84,69 @@ medianOfThree:
 
 	### INSERT YOUR CODE HERE
 	addiu $sp, $sp, -24 # Allocate space for return address
-	sw $ra, 8($sp) # Push return address onto stack
-	sw $fp,4($sp)
-	sw $a1,12($sp)
-	sw $a2,16($sp)
-	sw $a0,20($sp)
-	addiu $fp,$sp,24
+	sw $ra, 20($sp) 	#Push return address onto stack
+	sw $fp, 16($sp)
+	sw $s1, 12($sp)
+	sw $s2,  8($sp)
+	sw $s0,  4($sp)		
+	addiu $fp, $sp, 24
+	
 	#Opertation begins
-	sll $t1, $a1,2#multiply a1 by 4
-	sll $t2, $a2,2 #multiply a2 by 4
-	add $t1,$a0,$t1#get location for x[a1]=x[lo]
-	add $t2,$a0,$t2#get location for x[a2]=x[hi]
+	sll $s1, $a1, 2		#Multiply a1 by 4
+	sll $s2, $a2, 2		#Multiply a2 by 4
+	move $s0, $a0		#Base address stored in $s0
+	add $s1, $s0, $s1	#Get address for x[a1]=x[lo]
+	add $s2, $s0, $s2	#Get address for x[a2]=x[hi]
+	lw $t1, 0($s1)		#Get element of lo
+	lw $t2, 0($s2)		#Get element of hi
 	
-	#get location for x[mid] as 
-	add $t0,$a1,$a2#a1+a2/2
-	srl $t0,$t0,1#shift right arithmetic to sign extend the value
-	mflo $t0#round down 
-	add $t8, $t0, $zero# t8 contains the real mid value
-	sll $t0,$t0,2#multiply mid by4
-	add $t6, $a0, $t0#address for x[mid]
+	#Get address for x[mid] 
+	add $t0, $t1, $t2	#(lo element + hi element)
+	srl $t0, $t0, 1		#shift right arithmetic to sign extend the value
+	mflo $t0		#round down 
 	
-	lw $t3,0($t1)#access value of x[lo]
-	lw $t4,0($t2)#access value of x[hi]
-	lw $t5,0($t6)#access value of x[mid]
-	slt $t7, $t4,$t3
-	bne $t7,$zero, case1Med	
-case1Med:  addi $a1, $a1,0
-	addi $a2, $a2,0
+	slt $t3, $t1, $t2	#compare x[lo] with x[hi]
+	bne $t7, $zero, case1Med	
+	
+case1Med:  
+	addi $a1, $t1,0
+	addi $a2, $t2,0
 	jal swap
-	lw $a0, 20($sp)
 	lw $a1, 12($sp)
-	lw $a2, 16($sp)
-	slt $t7, $t4,$t5
-	bne $t7,$zero, case2Med	
-case2Med:  addi $a1, $t8,0
-	addi $a2, $a2,0
+	lw $a2,  8($sp)
+	lw $a0,  4($sp)		
+	slt $t3, $t2, $t0	#compare x[hi] with x[mid]
+	bne $t3 ,$zero, case2Med	
+case2Med:  
+	addi $a1, $t0,0
+	addi $a2, $t2,0
 	jal swap
-	lw $a0, 20($sp)
 	lw $a1, 12($sp)
-	lw $a2, 16($sp)
-	slt $t7, $t5,$t3
-	bne $t7,$zero, case3Med
-case3Med:  addi $a1, $a1,0
-	addi $a2, $t8,0
+	lw $a2,  8($sp)
+	lw $a0,  4($sp)
+	slt $t3, $t0, $t1	#compare x[mid] with x[lo]
+	bne $t3, $zero, case3Med
+case3Med:  
+	addi $a1, $t1,0
+	addi $a2, $t0,0
 	jal swap
-	lw $a0, 20($sp)
 	lw $a1, 12($sp)
-	lw $a2, 16($sp)
- 	addi $a1, $a1,0
-	addi $a2, $t8,0
-	lw $a0, 20($sp)
-	lw $a1, 12($sp)
-	lw $a2, 16($sp)
+	lw $a2,  8($sp)
+	lw $a0,  4($sp)
+	
+ 	addi $a1, $t1, 0
+	addi $a2, $t0, 0
+	jal swap
 
-	lw $ra, 8($sp) # Push return address onto stack
-	lw $fp,4($sp)
-	lw $a1,12($sp)
-	lw $a2,16($sp)
-	lw $a0,20($sp)
-	addiu $sp, $sp, 24 #clear up stack and return everything in position for caller
+	lw $ra, 20($sp) 	
+	lw $fp, 16($sp)
+	lw $a1, 12($sp)
+	lw $a2,  8($sp)
+	lw $a0,  4($sp)		
+	addiu $fp, $sp, 24
+	
 	# return to caller	
-   jr $ra
+   	jr $ra
    ##############################################
    #MEDIAN OF THREE TEST FAILED
    ##################################################
